84085f2cc06fba7a5903f3be0caaaa222f762370
Jose V. Herrera Gallegos <josevictorherrera@gmail.com>
2024-05-21 17:59:42 -0700

    i18n updates

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,7 @@
 module.exports = function() {
     return {
+        url: process.env.URL || 'http://localhost:8080',
+        siteName: 'Jose Herrera Portfolio',
         // https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
         environment: (process.env.NETLIFY || "false") == "true" ? "production" : "development",
     };
